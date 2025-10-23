1fe489e664c74f0dc0c7cb92856a11a04fbd6b7b
HerbTeaCup <132678572+HerbTeaCup@users.noreply.github.com>
2025-10-23 21:29:36 +0900

    한글 적용

---
+++ Assets/Scripts/LLM/Gemini/API/SystemSetting.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//보안상 좋지 않은 선택이지만, 일단 편의를 위해 ScriptableObject로 구현
+//나중에 서버 중개, 암호화 등 보안 강화 필요
+public class SystemSetting : ScriptableObject
+{
+
+}
